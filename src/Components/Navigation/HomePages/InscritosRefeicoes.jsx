a57e7f94ff74ef8ff7e5736fdd8fa1976602026b
Joao Dumba <30002076@students.ual.pt>
2024-12-24 16:25:02 +0000

    birthday

--- src/Components/Navigation/HomePages/InscritosRefeicoes.jsx
+++ src/Components/Navigation/HomePages/InscritosRefeicoes.jsx
@@ -75,8 +75,8 @@ const InscritosRefeicoes = () => {
         const formatarData = (data) => {
             const dia = data.getDate().toString().padStart(2, '0');
             const mes = (data.getMonth() + 1).toString().padStart(2, '0');
-            const ano = data.getFullYear();
-            return `${dia}/${mes}/${ano}`;
+            //const ano = data.getFullYear();
+            return `${dia}/${mes}`; // /${ano}
         };
 
         const dataAtualFormatada = formatarData(dataAtual);
